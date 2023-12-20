ae90efa656d1b561ec90d3a92f15a31681076764
Anshul0707 <anshul.markwade786@gmail.com>
2023-12-20 16:02:04 +0530

    code refactor

--- src/main/java/com/employee/management/Service/FileHandler.java
+++ src/main/java/com/employee/management/Service/FileHandler.java
@@ -3,12 +3,13 @@ package com.employee.management.Service;
 import com.employee.management.model.Employee;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class FileHandler {
     private static final String JSON_FILE_PATH = "src/main/resources/Data/employees.json";
     private static final File file = new File(JSON_FILE_PATH);
