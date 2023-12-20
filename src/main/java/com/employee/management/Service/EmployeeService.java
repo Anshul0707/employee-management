8cf071c6c0c4fd45e38c71b8721312424bc9b115
Anshul0707 <anshul.markwade786@gmail.com>
2023-12-20 12:45:13 +0530

    code refactor..

--- src/main/java/com/employee/management/Service/EmployeeService.java
+++ src/main/java/com/employee/management/Service/EmployeeService.java
@@ -1,36 +1,22 @@
 package com.employee.management.Service;
 
 import com.employee.management.model.Employee;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private static final String JSON_FILE_PATH = "src/main/resources/Data/employees.json";
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    static File file = new File(JSON_FILE_PATH);
     private long nextId = 1;
 
-    static {
-        File parentDir = file.getParentFile();
-        if (!parentDir.exists()) {
-            parentDir.mkdirs();
-        }
-    }
+    @Autowired
+    private FileHandler fileHandler;
+
 
     public List<Employee> getAllEmployees() {
-        try {
-            return objectMapper.readValue(file, new TypeReference<List<Employee>>() {
-            });
-        } catch (IOException e) {
-            return new ArrayList<>();
-        }
+        return FileHandler.readEmployeesFromFile();
     }
 
     public Employee getEmployeeById(Long id) {
@@ -46,40 +32,27 @@ public class EmployeeService {
         List<Employee> employees = getAllEmployees();
         employee.setId(nextId++);
         employees.add(employee);
-        saveEmployees(employees);
+        FileHandler.writeEmployeesToFile(employees);
     }
 
     public void updateEmployee(Long id, Employee updatedEmployee) {
         deleteEmployee(id);
         List<Employee> employees = getAllEmployees();
-//        employees.removeIf(employee -> employee.getId().equals(id));
         updatedEmployee.setId(id);
         employees.add(updatedEmployee);
-        saveEmployees(employees);
+        FileHandler.writeEmployeesToFile(employees);
     }
 
     public void deleteEmployee(Long id) {
         List<Employee> employees = getAllEmployees();
         List<Employee> removeEmployees = new ArrayList<>();
-//        employees.removeIf(employee -> employee.getId().equals(id));
         for (Employee employee : employees) {
             if (employee.getId().equals(id)) {
                 removeEmployees.add(employee);
             }
         }
         employees.removeAll(removeEmployees);
-        saveEmployees(employees);
-    }
-
-    private void saveEmployees(List<Employee> employees) {
-        try {
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            objectMapper.writeValue(file, employees);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FileHandler.writeEmployeesToFile(employees);
     }
 }
 
