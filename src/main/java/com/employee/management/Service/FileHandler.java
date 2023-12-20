19d823c9c6047acfe9ae0dc01e4f285cd3578afa
Anshul0707 <anshul.markwade786@gmail.com>
2023-12-20 12:46:37 +0530

    create separate java file to handel the file .

---
+++ src/main/java/com/employee/management/Service/FileHandler.java
@@ -0,0 +1,45 @@
+package com.employee.management.Service;
+
+import com.employee.management.model.Employee;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+    private static final String JSON_FILE_PATH = "src/main/resources/Data/employees.json";
+    private static final File file = new File(JSON_FILE_PATH);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        File parentDir = file.getParentFile();
+        if (!parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+    }
+
+    public static List<Employee> readEmployeesFromFile() {
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+                return new ArrayList<>();
+            }
+            return objectMapper.readValue(file, new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    public static void writeEmployeesToFile(List<Employee> employees) {
+        try {
+            objectMapper.writeValue(file, employees);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
